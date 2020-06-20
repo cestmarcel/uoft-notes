84a09a513b5c4e7586a10253708c88f1c765a1ff
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-06-19 20:24:24 -0400

    Updated server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('../../'))
 
